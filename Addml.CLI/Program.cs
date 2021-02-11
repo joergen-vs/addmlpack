f1fad91fcf9408f7aa3b32c2543ac252c53a6101
Jørgen Østengen Vik-Strandli <jorvik@arkivverket.no>
2021-02-11 03:36:03 +0100

    Added simple CLI for testing

---
+++ Addml.CLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Addml.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Addml.API.API.Run(args);
+            Console.Read();
+        }
+    }
+}
